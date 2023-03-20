aa80c9838dac6a876aa127e4664cda6cbd8c0526
pooranikarunanithi <pooranikarunanithi09@gmail.com>
2023-03-20 14:57:31 +0100

    second commit

--- converter/settings.py
+++ converter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%z#k6javfz3u9x1-!+1ci$%h(w=3ct56iehc6wc_!gd==g^#&e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
 
 
 # Application definition
